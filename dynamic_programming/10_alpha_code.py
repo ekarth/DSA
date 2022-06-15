5001334308908832bba44195e252b6d64eaf3b17
Ekarth <ekarthjain06@gmail.com>
2022-06-15 16:38:57 +0530

    added function description

--- dynamic_programming/10_alpha_code.py
+++ dynamic_programming/10_alpha_code.py
@@ -1,5 +1,7 @@
 mod = 10**9 + 7
 def num_codes(number):
+    '''Input: a string of numbers
+    Output: returns number of possible words that can be formed from given number'''
     answer = [0] * (len(number)+1)
     answer[0] = 1
     answer[1] = 1
@@ -7,7 +9,7 @@ def num_codes(number):
         answer[i] = answer[i-1]
         if(int(number[i-2]) * 10 + int(number[i-1]) <= 26):
             answer[i] += answer[i-2]
-    return answer[-1]
+    return answer[-1] % mod
 
 print(num_codes(str(25114)))
 print(num_codes(str(1111111111)))
