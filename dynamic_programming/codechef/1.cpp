48b64144372c55700762211dfc6a2ca0b814ef62
Ekarth <ekarthjain06@gmail.com>
2022-06-24 07:50:15 +0530

    Remove counting for loop

--- dynamic_programming/codechef/1.cpp
+++ dynamic_programming/codechef/1.cpp
@@ -8,21 +8,16 @@ long long countSubarrays(long long arr[], int n)
 {
     long long count[n];
     count[0] = 1;
+    long long total_count = 1;
     for(int i = 1; i < n; i++)
     {
         if(arr[i] >= arr[i-1]) // if non-dec then currnt element will be at the end of prev subarrays + 1 (as it is a subarray alone)
-        {
             count[i] = count[i-1] + 1;
-        }
         else // else it is the only subarray
-        {
             count[i] = 1;
-        }
+        total_count += count[i];
     } 
-    long long total_count = 0;
-    // for(int i = 0; i < n; i++) cout << count[i] << " ";
-    // cout << endl;
-    for(int i = 0; i < n; i++) total_count += count[i];
+    
     return total_count;
 
 }
