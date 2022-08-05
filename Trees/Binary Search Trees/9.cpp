fe884f0b49e759158da1edcc964642e4e6facd25
Ekarth <ekarthjain06@gmail.com>
2022-08-05 11:35:00 +0530

    Preorder to BST

---
+++ Trees/Binary Search Trees/9.cpp
@@ -0,0 +1,203 @@
+// Link: https://practice.geeksforgeeks.org/problems/preorder-to-postorder4423/1
+
+/* A O(n) iterative program for construction of BST from preorder traversal */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct Node
+{
+    int data;
+    struct Node *left, *right;
+} Node;
+
+// A Stack has array of Nodes, capacity, and top
+typedef struct Stack
+{
+    int top;
+    int capacity;
+    Node* *array;
+} Stack;
+
+// A utility function to create a new tree node
+Node* newNode( int data )
+{
+    Node* temp = (Node *)malloc( sizeof( Node ) );
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// A utility function to create a stack of given capacity
+Stack* createStack( int capacity )
+{
+    Stack* stack = (Stack *)malloc( sizeof( Stack ) );
+    stack->top = -1;
+    stack->capacity = capacity;
+    stack->array = (Node **)malloc( stack->capacity * sizeof( Node* ) );
+    return stack;
+}
+
+// A utility function to check if stack is full
+int isFull( Stack* stack )
+{
+    return stack->top == stack->capacity - 1;
+}
+
+// A utility function to check if stack is empty
+int isEmpty( Stack* stack )
+{
+    return stack->top == -1;
+}
+
+// A utility function to push an item to stack
+void push( Stack* stack, Node* item )
+{
+    if( isFull( stack ) )
+        return;
+    stack->array[ ++stack->top ] = item;
+}
+
+// A utility function to remove an item from stack
+Node* pop( Stack* stack )
+{
+    if( isEmpty( stack ) )
+        return NULL;
+    return stack->array[ stack->top-- ];
+}
+
+// A utility function to get top node of stack
+Node* peek( Stack* stack )
+{
+    return stack->array[ stack->top ];
+}
+
+bool canRepresentBST(int pre[], int n)
+{
+    // Create an empty stack
+    stack<int> s;
+
+    // Initialize current root as minimum possible
+    // value
+    int root = INT_MIN;
+
+    // Traverse given array
+    for (int i=0; i<n; i++)
+    {
+        // If we find a node who is on right side
+        // and smaller than root, return false
+        if (pre[i] < root)
+            return false;
+
+        // If pre[i] is in right subtree of stack top,
+        // Keep removing items smaller than pre[i]
+        // and make the last removed item as new
+        // root.
+        while (!s.empty() && s.top()<pre[i])
+        {
+            root = s.top();
+            s.pop();
+        }
+
+        // At this point either stack is empty or
+        // pre[i] is smaller than root, push pre[i]
+        s.push(pre[i]);
+    }
+    return true;
+}
+
+
+// A utility function to print postorder traversal of a Binary Tree
+void printPostorder (Node* node)
+{
+    if (node == NULL)
+        return;
+    printPostorder(node->left);
+    printPostorder(node->right);
+    printf("%d ", node->data);
+}
+
+Node* newNode( int data )
+{
+    Node* temp = (Node *)malloc( sizeof( Node ) );
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+class Solution{
+public:
+    // Approach 1: whenever root is NULL, create root and return it.
+    Node* helper(Node *root, int &val)
+    {
+        if(root == NULL)
+        {
+            root = newNode(val);
+            return root;
+        }
+        if(root->data > val) root->left = helper(root->left, val);
+        else if(root->data < val) root->right = helper(root->right, val);
+
+    }
+
+    // Approach 2: At each step divide the pre array in two parts left and right, and process them both
+    Node* insert(int mini, int maxi, int pre[], int start, int end)
+    {
+        if(start == end) return NULL;
+        Node *temp = NULL;
+        if(pre[start] < maxi && pre[start] > mini)
+        {
+            temp = newNode(pre[start]);
+            int i = start+1;
+            for(; i < end; i++)
+            {
+                if(pre[i] > pre[start]) break;
+            }
+            temp->left = insert(mini, temp->data, pre, start+1, i);
+            temp->right = insert(temp->data, maxi, pre, i, end);
+        }
+        return temp;
+    }
+    Node* post_order(int pre[], int size)
+    {
+        Node *root = newNode(pre[0]);
+        // for(int i = 1; i < size; i++)
+        // {
+        //     helper(root, pre[i]);
+            
+        // }
+        int i = 1;
+        for(; i < size; i++)
+        {
+            if(pre[i] > root->data) break;
+        }
+        root->left = insert(INT_MIN, root->data, pre, 1, i);
+        root->right = insert(root->data, INT_MAX, pre, i, size);
+        return root;
+    }
+};
+
+int main()
+{
+    // Note that size of arr[] is considered 100 according to
+    // the constraints mentioned in problem statement.
+    int arr[1000], x, t, n;
+
+    // Input the number of test cases you want to run
+    cin >> t;
+
+    // One by one run for all input test cases
+    while (t--)
+    {
+        // Input the size of the array
+        cin >> n;
+
+        // Input the array
+        for (int i=0; i<n; i++)
+            cin >> arr[i];
+        
+        Solution ob;
+        printPostorder(ob.post_order(arr, n));
+        cout << endl;
+    }
+    return 0;
+}
